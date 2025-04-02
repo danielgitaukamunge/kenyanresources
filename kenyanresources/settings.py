24a921c0f68358d23067f3557a49982871518728
danielgitaukamunge <dankamunge1111@gmail.com>
2025-04-02 21:04:32 +0300

    All Hosts

--- kenyanresources/settings.py
+++ kenyanresources/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
-ALLOWED_HOSTS = ['kenyanresources-b61830586c6e.herokuapp.com']
+ALLOWED_HOSTS = ['kenyanresources-b61830586c6e.herokuapp.com', '*',]
 
 # Database
 DATABASES = {
